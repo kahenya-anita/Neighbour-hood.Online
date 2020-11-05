a48d4ec94b20dfe93fb1d38e1b13ba65e821e20d
kahenya-anita <anitakahenya1@gmail.com>
2020-11-06 00:40:02 +0300

    Installed all the requirements

--- NBHapp/urls.py
+++ NBHapp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     
     
     path('', views.home, name = 'home'),
-    path('hood/', views.Dashboard, name = 'Dashboard'),
+    path('hood/', views.hood, name = 'hood'),
     path('create_neighbourhood/', views.create_neighbourhood, name = 'create_neighbourhood'),
     path('single_neighbourhood/', views.single_neighbourhood, name = 'single_neighbourhood'),
     path('create_business/', views.create_business, name = 'create_business')
--- NBHapp/views.py
+++ NBHapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
