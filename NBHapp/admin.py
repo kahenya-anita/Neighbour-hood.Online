9cfb16d9ed6a136f05eadbc3d5eb2127678e4108
kahenya-anita <anitakahenya1@gmail.com>
2020-11-06 03:52:41 +0300

    Registered models in Admin

--- NBHapp/admin.py
+++ NBHapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Profile)
+
